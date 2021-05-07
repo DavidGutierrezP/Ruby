6ca114075ad10b861d575324e09a68729388ecce
DavidShrimp <agutierrezp1701@alumno.ipn.mx>
2021-05-07 13:13:31 -0500

    Funciones y clases

---
+++ Methods_Classes.rb
@@ -0,0 +1,20 @@
+#Métodos y clases en Ruby
+
+def acelerar
+    puts "Frenando en la estación de gasolina"
+    puts "Aumentando la velocidad!"
+end
+
+def claxon
+    puts "Presionando el claxon"
+    puts "Beep Beep"
+end
+
+def faros(brillo)
+    puts "Prendiendo los faros con un brillo del #{brillo} por ciento"
+    puts "Cuidado con el venado!"
+end
+
+acelerar
+claxon
+faros("45")
